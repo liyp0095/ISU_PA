6096a57fca23a6d2d0888a51454c361f6c885905
Yuepei Li <ypli@yuepeidembp.student.iastate.edu>
2019-09-13 11:57:05 -0500

    change name

---
+++ 2019F/CS552/Assignment1/pm4.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(){
+  int i;
+  printf("A\n");
+  for(i=0;i<3;i++) {
+    if(i%2==1) {
+      fork();
+    } else {
+      fork();
+      fork();
+    }
+  }
+}
--- 2019F/CS552/Assignment1/run.sh
+++ 2019F/CS552/Assignment1/run.sh
@@ -6,6 +6,6 @@ C_FILE_NAME=${C_FILE%.*}
 OUT_FILE=$C_FILE_NAME.out
 gcc $C_FILE -o $OUT_FILE
 #rm ${C_FILE_NAME}_output.txt
-./$OUT_FILE 
+./$OUT_FILE
 #> ${C_FILE_NAME}_output.txt
 rm $OUT_FILE
