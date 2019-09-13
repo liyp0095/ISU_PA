9e560070fc4e302adcf671750520de1663c8ea79
liyp0095 <liyp0095@pyrite-n4.cs.iastate.edu>
2019-09-13 11:18:56 -0500

    > will make difference?

--- 2019F/CS552/Assignment1/Pr3_1.c
+++ 2019F/CS552/Assignment1/Pr3_1.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 int main()
 {
-  for(int i=0;i<3;i++)
-    if(fork()>0)
+  for(int i=0;i<3;i++) {
+    printf("i=%d\n", i);
+    if(fork()>0) {
       printf("A\n");
+    }
+  }
 }
--- 2019F/CS552/Assignment1/run.sh
+++ 2019F/CS552/Assignment1/run.sh
@@ -5,5 +5,7 @@ C_FILE_NAME=${C_FILE%.*}
 
 OUT_FILE=$C_FILE_NAME.out
 gcc $C_FILE -o $OUT_FILE
-./$OUT_FILE > ${C_FILE_NAME}_output.txt
+#rm ${C_FILE_NAME}_output.txt
+./$OUT_FILE 
+#> ${C_FILE_NAME}_output.txt
 rm $OUT_FILE
