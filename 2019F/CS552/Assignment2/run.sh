07fd1cc64d778aa6ea74b677f3479940fb6b82cd
Yuepei Li <ypli@yuepeidembp.student.iastate.edu>
2019-09-27 12:43:19 -0500

    add run.sh para

--- 2019F/CS552/Assignment2/run.sh
+++ 2019F/CS552/Assignment2/run.sh
@@ -7,5 +7,5 @@ C_FILE_NAME=${C_FILE%.*}
 OUT_FILE=$C_FILE_NAME.out
 gcc $C_FILE -o $OUT_FILE
 #rm ${C_FILE_NAME}_output.txt
-./$OUT_FILE # > output_${C_FILE_NAME}.txt
+./$OUT_FILE $C_PARA # > output_${C_FILE_NAME}.txt
 rm $OUT_FILE
