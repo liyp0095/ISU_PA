db4aab82030cb72cadec268f06538ff5c88bb9d2
Yuepei Li <ypli@yuepeidembp.student.iastate.edu>
2019-09-28 15:21:24 -0500

    add

--- 2019F/CS552/Assignment2/run.sh
+++ 2019F/CS552/Assignment2/run.sh
@@ -5,7 +5,7 @@ C_PARA="rrwwrwwr"
 C_FILE_NAME=${C_FILE%.*}
 
 OUT_FILE=$C_FILE_NAME.out
-gcc $C_FILE -o $OUT_FILE
+gcc $C_FILE -lpt -o $OUT_FILE
 #rm ${C_FILE_NAME}_output.txt
 ./$OUT_FILE $C_PARA # > output_${C_FILE_NAME}.txt
 rm $OUT_FILE
