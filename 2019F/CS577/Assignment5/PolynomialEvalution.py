676486b28da999da3459f586468de537792dca52
liyp0095 <liyp0095@gmail.com>
2019-10-09 22:40:57 -0500

    first two question of assignment5 cs577

---
+++ 2019F/CS577/Assignment5/PolynomialEvalution.py
@@ -0,0 +1,27 @@
+#!/bin/python
+#-*- coding: utf8 -*-
+
+
+def evalPoly(a, t):
+    n = len(a) - 1
+    b = [0.0] * len(a)
+    c = [0.0] * len(a)
+    b[-1] = a[-1]
+    c[-1] = b[-1]
+    for k in range(n-1, 0, -1):
+        b[k] = a[k] + t*b[k+1]
+        c[k] = b[k] + t*c[k+1]
+    b[0] = a[0] + t*b[1]
+    return b[0], c[1]
+
+
+def main():
+    p1 = 1.414214
+    p2 = complex(1, 2)
+    a = [51200, 0, -39712, 0, 7392, 0, -170, 0, 1]
+    print(evalPoly(a, p1))
+    print(evalPoly(a, p2))
+
+
+if __name__ == '__main__':
+    main()
