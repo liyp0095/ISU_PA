34907056cd037fa037e2151df2a90852d4fb377a
liyp0095 <liyp0095@gmail.com>
2019-10-17 04:09:10 -0500

    test gui

--- 2019F/CS572/Lab2/draughts.py
+++ 2019F/CS572/Lab2/draughts.py
@@ -9,9 +9,18 @@ sys.path.append("../aima_python")
 # Game = importlib.import_module("aima-python.games")
 from aima_python.games import Game
 
+import wx
+
+class Draughts:
+    def __init__(self):
+        app = wx.App()
+        frame = wx.Frame(None, style=wx.MAXIMIZE_BOX | wx.RESIZE_BORDER
+	           | wx.SYSTEM_MENU | wx.CAPTION |	 wx.CLOSE_BOX)
+        frame.Show()
+        app.MainLoop()
 
 def main():
-    g = Game()
+    d = Draughts()
 
 
 if __name__ == '__main__':
--- 2019F/CS572/Lab2/test/test.py
+++ 2019F/CS572/Lab2/test/test.py
@@ -1,13 +1,123 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# simple.py
+"""
+ZetCode wxPython tutorial
+
+This program draws nine coloured rectangles
+on the window.
+
+author: Jan Bodnar
+website: zetcode.com
+last edited: May 2018
+"""
 
 import wx
 
-app = wx.App()
+class Example(wx.Frame):
 
-frame = wx.Frame(None, title='Simple application')
-frame.Show()
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
 
-app.MainLoop()
+        self.InitUI()
+
+    def InitUI(self):
+
+        wx.StaticText(self, label='x:', pos=(10,10))
+        wx.StaticText(self, label='y:', pos=(10,30))
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_MOVE, self.OnMove)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+
+        self.SetTitle("Colours")
+        self.Centre()
+        self.SetSize(600, 400)
+
+
+    def OnMove(self, e):
+        pass
+
+
+    def OnSize(self, e):
+        self.Refresh()
+
+
+    def OnPaint(self, e):
+
+        state = [(1,2), (7,6), (4, 4)]
+        x, y = self.GetSize()
+        y = y - 33
+        width = min(x, y)
+        cell_width = width / 8
+
+        dc = wx.PaintDC(self)
+        dc.SetPen(wx.Pen('#d4d4d4'))
+
+        dc.SetBrush(wx.Brush('#f0f0f0'))
+        dc.DrawRectangle(max(0, (x-y)/2), max(0, (y-x)/2), width, width)
+
+        dc.SetPen(wx.Pen('#dc4c4c'))
+        dc.SetBrush(wx.Brush('#dc4c4c', wx.SOLID))
+        for i in range(8):
+            for j in range(8):
+                if (i + j) % 2 == 0:
+                    dc.DrawRectangle(max(0, (x-y)/2)+i*cell_width,
+                                max(0, (y-x)/2)+j*cell_width,
+                                cell_width, cell_width)
+
+        dc.SetPen(wx.Pen('#555'))
+        dc.SetBrush(wx.Brush('#555'))
+        # dc.DrawCircle(30, 30, 10)
+        r = cell_width / 2.8
+        for p in state:
+            dc.DrawCircle(max(0, (x-y)/2) + cell_width * (p[0]-0.5),
+                        max(0, (y-x)/2) + cell_width * (p[1]-0.5), r)
+
+        # dc.SetBrush(wx.Brush('#000000'))
+        # dc.DrawRectangle(0.1*x, 0.1*y, 0.8*x, 0.8*y)
+
+        # dc.SetBrush(wx.Brush('#c56c00'))
+        # dc.DrawRectangle(0, 0, x, y)
+        #
+        # dc.SetBrush(wx.Brush('#c56c00'))
+        # dc.DrawRectangle(0, 0, x, y)
+
+        # dc.SetBrush(wx.Brush('#c56c00'))
+        # dc.DrawRectangle(0.1*x, 15, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#1ac500'))
+        # dc.DrawRectangle(130, 15, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#539e47'))
+        # dc.DrawRectangle(250, 15, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#004fc5'))
+        # dc.DrawRectangle(10, 105, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#c50024'))
+        # dc.DrawRectangle(130, 105, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#9e4757'))
+        # dc.DrawRectangle(250, 105, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#5f3b00'))
+        # dc.DrawRectangle(10, 195, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#4c4c4c'))
+        # dc.DrawRectangle(130, 195, 90, 60)
+        #
+        # dc.SetBrush(wx.Brush('#785f36'))
+        # dc.DrawRectangle(250, 195, 90, 60)
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
