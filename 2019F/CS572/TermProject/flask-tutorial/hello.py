59929293de9ab8fa76f9ebf57c59d7b3c715cece
liyp0095 <liyp0095@gmail.com>
2019-11-06 22:35:34 -0600

    add flask

--- 2019F/CS572/TermProject/flask-tutorial/hello.py
+++ 2019F/CS572/TermProject/flask-tutorial/hello.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+
+@app.route('/')
+@app.route('/board')
+def board():
+    return render_template('board.html', title='Home')
+
+
+if __name__ == "__main__":
+    app.run()
