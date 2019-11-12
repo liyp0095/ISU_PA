cbec9285a3e21aa9eafd0b5f5787aa5c22442471
liyp0095 <liyp0095@gmail.com>
2019-11-11 22:01:58 -0600

    add

--- 2019F/CS572/TermProject/flask-tutorial/hello.py
+++ 2019F/CS572/TermProject/flask-tutorial/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request, jsonify
 from flask import render_template
 
 app = Flask(__name__, template_folder='templates')
@@ -18,6 +19,17 @@ def board():
 @app.route('/test')
 def test():
     return render_template('t1.html', title='Home')
+
+
+# ajax，Get方式与js交互(非表单）
+@app.route('/_add_numbers')
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    # log.info(a)
+    # log.info(b)
+    return jsonify(result=a + b)
 
 
 if __name__ == "__main__":
