e2cc1ce349bc3361f11e6ff53bde1ab3b216a3ee
liyp0095 <liyp0095@gmail.com>
2019-11-13 17:08:18 -0600

    finish hw9 577

--- 2019F/CS577/Assignment9/README.md
+++ 2019F/CS577/Assignment9/README.md
@@ -2,10 +2,111 @@
 
 ## Platform
 
-
+python 3.7
 
 ## How to run
 
+```shell
+# change the initial value of x1,x2 in main function.
+python main.py  
+
+```
+
 ## Result
 
+```sh
+# sample result 1
+(base) ➜  Assignment9 git:(master) ✗ python main.py
+================= minima ==================
+starts at (0.000000, 0.000000)
+1 th iteration:
+ * t: 0.5774
+ * position: (1.7321, 0.0)
+ * f_value: -3.4641
+ * accuracy: 3.4641
+2 th iteration:
+ * t: 0.0
+ * position: (1.7321, 0.0)
+ * f_value: -3.4641
+ * accuracy: 0.0
+================= maxima ==================
+starts at (0.000000, 0.000000)
+1 th iteration:
+ * t: -0.5774
+ * position: (-1.7321, 0.0)
+ * f_value: 3.4641
+ * accuracy: -3.4641
+2 th iteration:
+ * t: 0.0
+ * position: (-1.7321, 0.0)
+ * f_value: 3.4641
+ * accuracy: 0.0
+
+# sample result 2
+(base) ➜  Assignment9 git:(master) ✗ python main.py
+================= minima ==================
+starts at (1.000000, 1.000000)
+1 th iteration:
+ * t: 0.548
+ * position: (1.548, -0.096)
+ * f_value: -3.3932
+ * accuracy: 1.7266
+2 th iteration:
+ * t: 0.3123
+ * position: (1.7336, -0.0032)
+ * f_value: -3.4641
+ * accuracy: 0.0708
+3 th iteration:
+ * t: 0.2883
+ * position: (1.732, -0.0)
+ * f_value: -3.4641
+ * accuracy: 0.0
+4 th iteration:
+ * t: 0.2887
+ * position: (1.7321, -0.0)
+ * f_value: -3.4641
+ * accuracy: 0.0
+================= maxima ==================
+starts at (1.000000, 1.000000)
+1 th iteration:
+ * t: -0.7018
+ * position: (0.2982, 2.4037)
+ * f_value: 0.837
+ * accuracy: -2.5037
+2 th iteration:
+ * t: 0.7924
+ * position: (-1.9735, 1.2679)
+ * f_value: 0.186
+ * accuracy: 0.651
+3 th iteration:
+ * t: -0.2201
+ * position: (-1.4228, 0.1666)
+ * f_value: 3.2689
+ * accuracy: -3.0828
+4 th iteration:
+ * t: -0.3313
+ * position: (-1.7368, 0.0096)
+ * f_value: 3.4639
+ * accuracy: -0.195
+5 th iteration:
+ * t: -0.2876
+ * position: (-1.732, 0.0)
+ * f_value: 3.4641
+ * accuracy: -0.0002
+6 th iteration:
+ * t: -0.2887
+ * position: (-1.7321, 0.0)
+ * f_value: 3.4641
+ * accuracy: -0.0
+```
+
 ## Problem
+
+- [ ] In some cases, no real root for quadratic equation of t
+- [ ] How to choose start position?
+
+## Learn
+
+https://www.geogebra.org/3d?lang=en, this link gives 3d graph
+
+![3d graph of function f](./image/3dGraph.png)
--- 2019F/CS577/Assignment9/main.py
+++ 2019F/CS577/Assignment9/main.py
@@ -9,14 +9,16 @@ Date: 2019-11-12
 '''
 
 accuracy = 10 ** (-6)
+decimal_accuracy = 4
 
 def getT(x1, x2):
+    "return t1, t2 that make gradient of t equal 0"
     f_x1 = x1**2 + x2**2 - 3
     f_x2 = 2*x1*x2
     a = 3*(-f_x2**2*f_x1 - (1/3.0)*f_x1**3)
     b = 2*(f_x1**2*x1 + f_x2**2*x1 + 4*x1*x2**2*f_x1)
     c = -f_x1 * x1**2 - f_x2**2 - x2**2 * f_x1 + 3*f_x1
-
+    # print(a, b, c)
     delta = b**2 - 4*a*c
     t1 = (-b + delta ** (1/2.0)) / 2 / a
     t2 = (-b - delta ** (1/2.0)) / 2 / a
@@ -25,36 +27,65 @@ def getT(x1, x2):
 
 
 def f(x1, x2):
+    "function f"
     return x1**3/3.0 + x2**2 * x1 - 3*x1
 
 
 def fd(x1, x2):
+    "gradient of function f"
     return (x1**2 + x2**2 - 3, 2*x1*x2)
 
 
 def findExtremum(x1, x2, s = 1):
+    "steepest gradient method"
     d = 1
     fv = f(x1, x2)
+    steps = []
     while abs(d) > accuracy:
+        details = {}
         t = getT(x1, x2)
-        print(x1, x2)
-        print(t)
         gradient = fd(x1, x2)
+        # print(gradient)
         if s == 1:
+            "minima"
             x1 -= t[0] * gradient[0]
             x2 -= t[0] * gradient[1]
+            details["t"] = t[0]
         else:
+            "maxima"
             x1 -= t[1] * gradient[0]
             x2 -= t[1] * gradient[1]
+            details["t"] = t[1]
         d = fv - f(x1, x2)
         fv = f(x1, x2)
-        print(fv)
-        print()
+        details["accuracy"] = d
+        details["f_value"] = fv
+        details["position"] = (x1, x2)
+        steps.append(details)
+
+    return steps
+
+
+def show_steps(steps):
+    for i, s in enumerate(steps):
+        print(str(i+1) + " th iteration: ")
+        print(" * t: " + str(round(s["t"], decimal_accuracy)))
+        print(" * position: (%s, %s)" % (str(round(s["position"][0], decimal_accuracy)), str(round(s["position"][1], decimal_accuracy))))
+        print(" * f_value: " + str(round(s["f_value"], decimal_accuracy)))
+        print(" * accuracy: " + str(round(s["accuracy"], decimal_accuracy)))
 
 
 def main():
-    findExtremum(0,0, s = -1)
-    print(getT(1,1))
+    x1, x2 = 1, 1
+    print("================= minima ==================")
+    print("starts at (%f, %f)" % (x1, x2))
+    steps_min = findExtremum(x1, x2, s = 1)
+    show_steps(steps_min)
+    print("================= maxima ==================")
+    print("starts at (%f, %f)" % (x1, x2))
+    steps_max = findExtremum(x1, x2, s = -1)
+    show_steps(steps_max)
+    # print(getT(1,1))
     # pass()
 
 
