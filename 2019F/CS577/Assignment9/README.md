32de6f7173098923ffafc6e490e91e016f91fe64
liyp0095 <liyp0095@gmail.com>
2019-11-13 02:14:18 -0600

    add 577 hw9

---
+++ 2019F/CS577/Assignment9/README.md
@@ -0,0 +1,11 @@
+# CS577 Assignment9 by Yuepei Li
+
+## Platform
+
+
+
+## How to run
+
+## Result
+
+## Problem
---
+++ 2019F/CS577/Assignment9/main.py
@@ -0,0 +1,17 @@
+#!/bin/python
+# -*- coding: utf8 -*-
+
+'''
+Description: Use steepest descent and ascent to find the minima and maxima of
+            f(x1, x2) = x1^3/3 + x2^2*x1 - 3*x1
+Auther: Yuepei Li
+Date: 2019-11-12
+'''
+
+
+def main():
+    pass()
+
+
+if __name__ == "__main__":
+    main()
